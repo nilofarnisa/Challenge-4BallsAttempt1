398408586ce7d812bd8381c0db64ea2e9b172dd9
Nilofar Nisa <nilofarnisa28@gmail.com>
2021-02-25 14:12:05 +0530

    Code Modification for reducing duplicate method

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -5,7 +5,8 @@ public class TryProcessing extends PApplet{
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
     public static final int DIAMETER = 10;
-    int x=0,y=0,z=0,w=0;
+    int speed =1,x=0;
+
 
     public static void main(String[] args) {
         PApplet.main("TryProcessing",args);
@@ -24,34 +25,14 @@ public class TryProcessing extends PApplet{
 
     @Override
     public void draw() {
-       // paintWhite();
-        drawCircle();
-        drawCircle2();
-        drawCircle3();
-        drawCircle4();
-    }
-
-    private void drawCircle4() {
-        ellipse(w,(HEIGHT*4)/5, DIAMETER, DIAMETER);
-        w=w+4;
-    }
-
-    private void drawCircle3() {
-        ellipse(z,(HEIGHT*3)/5, DIAMETER, DIAMETER);
-        z=z+3;
-    }
-
-    private void drawCircle2() {
-        ellipse(y,(HEIGHT*2)/5, DIAMETER, DIAMETER);
-        y=y+2;
-    }
-
-    private void drawCircle() {
-        ellipse(x,HEIGHT/5, DIAMETER, DIAMETER);
+        drawCircle(speed,HEIGHT/5);
+        drawCircle(speed*2,HEIGHT*2/5);
+        drawCircle(speed*3,HEIGHT*3/5);
+        drawCircle(speed*4,HEIGHT*4/5);
         x++;
     }
 
-    private void paintWhite() {
-        background(255);
+    private void drawCircle(int speedOfBall,int position) {
+        ellipse(x*speedOfBall,position,DIAMETER,DIAMETER);
     }
 }
